948a685da9ab4b76594630a76dd2dc82f0004b9d
+ <+>
2024-03-20 20:04:08 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+const result = saturdayFun("bath my dog");
+ //monday work
+function mondayWork(task = "go to the office"){
+    return `This Monday, I will ${task}.`;
+}
+const mondayWorkResult = mondayWork("work from home.");
+
+function wrapAdjective(symbol){
+    return function(adjective){
+        return `You are ${symbol}${adjective}${symbol}!`;
+    };
+}
